e37b6ec9fbc58e97079fe4cccff6bcb2e8c9328f
Serhii1108 <68705512+Serhii1108@users.noreply.github.com>
2023-12-08 13:41:50 +0200

    feat: implement basic application structure

---
+++ gulp/tasks/script.js
@@ -0,0 +1,10 @@
+module.exports = function() {
+    $.gulp.task('scripts', function() {
+        return $.gulp.src('src/static/js/main.js')
+        .pipe($.gulp.dest('build/js/'))
+        .pipe($.bs.reload({
+            stream: true
+        }));
+    });
+
+};
